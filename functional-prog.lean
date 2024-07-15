d64e726619014fb4d2490a0b775e87af6fd8dc0c
Arpit Saxena <arpit.saxena2000@yahoo.in>
2024-07-15 13:07:20 +0530

    Complete functional programming lists section

--- functional-prog.lean
+++ functional-prog.lean
@@ -66,4 +66,110 @@ def add (a: ℕ) (b: ℕ) : ℕ :=
 instance IsAssociative_Add : IsAssociative add :=
   { assoc := add_assoc }
 
+-- Lists
+
+theorem head_head_cases {α : Type} [Inhabited α] (xs : List α):
+  head [head xs] = head xs :=
+  by
+    cases xs with
+    | nil => rfl
+    | cons x xs' => rfl
+
+theorem head_head_match {α : Type} [Inhabited α] (xs : List α):
+  head [head xs] = head xs :=
+    match xs with
+    | List.nil => by rfl
+    | List.cons x xs' => by rfl
+
+theorem injection_example {α: Type} (x y : α) (xs ys : List α)
+    (h : x :: xs = y :: ys) :
+  x = y ∧ xs = ys :=
+  by
+    cases h
+    simp
+
+theorem distinctness_example {α: Type} (y: α) (ys: List α) (h: [] = y :: ys) :
+  false :=
+  by cases h
+
+def map {α β: Type} (f : α -> β) (xs : List α) : List β :=
+  match xs with
+  | [] => []
+  | x :: xs => f x :: map f xs
+
+def mapArgs {α β: Type} : (α -> β) -> List α -> List β
+  | _, []       => []
+  | f, x :: xs  => f x :: mapArgs f xs
+
+theorem map_ident {α: Type} (xs : List α) :
+  map (fun x ↦ x) xs = xs :=
+  by
+    induction xs with
+    | nil => rfl
+    | cons x xs' ih => simp[ih, map]
+
+theorem map_comp {α β γ: Type} (f : α -> β) (g : β -> γ) (xs: List α) :
+  map g (map f xs) = map (fun x ↦ g (f x)) xs :=
+  by
+    induction xs with
+    | nil => rfl
+    | cons x xs' ih => simp[ih, map]
+
+theorem map_append (α β : Type) (f : α -> β) (xs ys : List α) :
+  map f (xs ++ ys) = (map f xs) ++ (map f ys) :=
+  by
+    induction xs with
+    | nil => rfl
+    | cons x xs' ih => simp[ih, map]
+
+def tail {α: Type} : List α -> List α
+  | [] => []
+  | _ :: xs => xs
+
+def headOpt {α: Type} : List α -> Option α
+  | [] => Option.none
+  | x :: _ => Option.some x
+
+def headPre {α: Type} : (xs : List α) -> xs ≠ [] -> α
+  | [], _ => by simp at *
+  | x :: _, _ => x
+
+#eval headPre [1, 2, 3] (by simp)
+
+def zip {α β : Type} : List α -> List β -> List (α × β)
+  | x :: xs, y :: ys => (x, y) :: zip xs ys
+  | []     , _       => []
+  | _ :: _ , []      => []
+
+def length {α : Type} : List α -> ℕ
+  | []        => 0
+  | _ :: xs   => 1 + length xs
+
+theorem min_add_add (a b c : ℕ) :
+  min (a + b) (a + c) = a + min b c :=
+  by
+    cases Classical.em (b ≤ c) with
+    | inl h => simp[h]
+    | inr h => simp[le_of_not_ge, h]
+
+theorem length_zip {α β : Type} (xs : List α) (ys : List β) :
+  length (zip xs ys) = min (length xs) (length ys) :=
+  by
+    induction xs generalizing ys with
+    | nil          => simp[zip, length]
+    | cons x xs ih =>
+      cases ys with
+      | nil         => simp[zip, length]
+      | cons y ys'  => simp[zip, length, min_add_add, ih ys']
+
+theorem map_zip {α α' β β' : Type} (f : α -> α') (g : β -> β') :
+  ∀xs ys,
+    map (fun ab : α × β ↦ (f (Prod.fst ab), g (Prod.snd ab))) (zip xs ys) =
+    zip (map f xs) (map g ys)
+    | x :: xs, y :: ys => by simp [zip, map, map_zip f g xs ys]
+    | []     , _       => by rfl
+    | _ :: _ , []      => by rfl
+
+
+
 end FunctionalProg
