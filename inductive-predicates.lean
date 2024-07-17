6e19e61182d4770502c31bce490096f58a073bb4
Arpit Saxena <arpit.saxena2000@yahoo.in>
2024-07-17 14:55:19 +0530

    Finish section 6.6 having examples

--- inductive-predicates.lean
+++ inductive-predicates.lean
@@ -140,4 +140,146 @@ theorem Even_iff_struct (n : ℕ) :
             by simp[Even.add_two, hm, heq]
     )
 
+-- Section 6.6.1 : Sorted lists
+
+inductive Sorted : List ℕ -> Prop where
+  | nil : Sorted []
+  | single (x : ℕ) : Sorted [x]
+  | two_or_more (x y : ℕ) {zs : List ℕ} (hle : x ≤ y) (hsorted : Sorted (y :: zs)) :
+    Sorted (x :: y :: zs)
+
+theorem Sorted_3_5 :
+  Sorted [3, 5] :=
+  by
+    apply Sorted.two_or_more
+    { simp }
+    { exact Sorted.single _ }
+
+theorem Sorted_3_t_raw :
+  Sorted [3, 5] :=
+  Sorted.two_or_more _ _ (by simp) (Sorted.single _)
+
+theorem Sorted_7_9_9_11 :
+  Sorted [7, 9, 9, 11] :=
+  Sorted.two_or_more _ _ (by simp) (
+    Sorted.two_or_more _ _ (by simp) (
+      Sorted.two_or_more _ _ (by simp) (
+        Sorted.single _
+      )
+    )
+  )
+
+theorem Not_Sorted_17_13 :
+  ¬ Sorted [17, 13] :=
+  by
+    intro h
+    cases h with
+    | two_or_more _ _ hle hsorted =>
+      simp at hle
+
+-- Section 6.6.2 : Palindromes
+
+inductive Palindrome {α : Type} : List α -> Prop where
+  | nil : Palindrome []
+  | single (x : α) : Palindrome [x]
+  | sandwich (x : α) {xs : List α} (hxs : Palindrome xs) : Palindrome ([x] ++ xs ++ [x])
+
+-- Already defined in forward-proofs.lean, but unable to figure out how to import it from there.
+def reverse {α : Type} (x : List α) : List α:=
+  match x with
+  | []      => []
+  | x :: xs => reverse xs ++ [x]
+
+theorem reverse_append {α : Type}:
+  ∀xs ys : List α,
+    reverse (xs ++ ys) = reverse ys ++ reverse xs :=
+  fun (xs: List α) => fun(ys: List α) =>
+  match xs with
+  | []      => by simp [reverse]
+  | x :: xs => by simp [reverse, reverse_append xs]
+
+theorem Palindrome_reverse {α : Type} (xs : List α) (hxs : Palindrome xs) :
+  Palindrome (reverse xs) :=
+  by
+    induction hxs with
+    | nil => exact Palindrome.nil
+    | single x => exact Palindrome.single x
+    | sandwich x hsx ih => {
+      rw [reverse_append]
+      exact Palindrome.sandwich x ih
+    }
+
+-- Section 6.6.3 : Full Binary Trees
+
+inductive BTree (α : Type) : Type where
+  | empty : BTree α
+  | node  : α -> BTree α -> BTree α -> BTree α
+
+inductive IsFull {α : Type} : BTree α -> Prop where
+  | empty : IsFull BTree.empty
+  | node (a : α) (l r : BTree α) (hl : IsFull l) (hr : IsFull r) (hiff : l = BTree.empty <-> r = BTree.empty) : IsFull (BTree.node a l r)
+
+theorem IsFull_singleton {α : Type} (a : α) :
+  IsFull (BTree.node a BTree.empty BTree.empty) :=
+  by
+    apply IsFull.node _ _ _ IsFull.empty IsFull.empty
+    rfl
+
+-- AGAIN, unnecessary copy where we should've been able to import
+def mirror {α: Type} : BTree α -> BTree α
+  | BTree.empty       => BTree.empty
+  | BTree.node a l r  => BTree.node a (mirror r) (mirror l)
+
+theorem mirror_Eq_empty_Iff {α: Type} :
+  ∀t : BTree α, mirror t = BTree.empty ↔ t = BTree.empty
+  | BTree.empty       => by rfl
+  | BTree.node _ _ _  => by simp[mirror]
+
+theorem IsFull_mirror {α : Type} (t : BTree α) (ht : IsFull t) :
+  IsFull (mirror t) :=
+  by
+    induction ht with
+    | empty => exact IsFull.empty
+    | node a l r hl hr hiff ih_l ih_r => {
+      rw [mirror]
+      apply IsFull.node
+      { exact ih_r }
+      { exact ih_l }
+      { simp [mirror_Eq_empty_Iff, hiff] }
+    }
+
+theorem IsFull_mirror_struct_induct {α : Type} (t : BTree α) :
+  IsFull t -> IsFull (mirror t) :=
+  by
+    induction t with
+    | empty => {
+      intro ht
+      exact ht
+    }
+    | node a l r ih_l ih_r => {
+      intro ht
+      cases ht with
+      | node a l r hl hr hiff => {
+        apply IsFull.node
+        { exact ih_r hr }
+        { exact ih_l hl }
+        { simp[mirror_Eq_empty_Iff, hiff] }
+      }
+    }
+
+-- Section 6.6.4 : First Order Terms
+
+inductive Term (α β : Type) : Type where
+  | var : β -> Term α β
+  | fn  : α -> List (Term α β) -> Term α β
+
+inductive WellFormed {α β : Type} (arity : α -> ℕ) :
+  Term α β -> Prop where
+  | var (x : β) : WellFormed arity (Term.var x)
+  | fn (f : α) (ts : List (Term α β)) (hargs: ∀t ∈ ts, WellFormed arity t)
+      (hlen: List.length ts = arity f) :
+    WellFormed arity (Term.fn f ts)
+
+-- skipped an inductive definition
+
 end InductivePredicates
